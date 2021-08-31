eb6061847f30471a546f2da2a638efe03c7cd33e
jeniferaaaaa <jeniferaaaaa@outlook.jp>
2021-08-31 19:49:42 +0900

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to リローディング.
+          Edit <code>src/App.js</code> and save to aリローディング.
         </p>
         <a
           className="App-link"
